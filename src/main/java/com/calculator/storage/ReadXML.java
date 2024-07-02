b84fb9b4949e7f7bd3e1addc734778ab24dce48b
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-07-02 09:11:10 +0200

    Fule add path

--- src/main/java/com/calculator/storage/ReadXML.java
+++ src/main/java/com/calculator/storage/ReadXML.java
@@ -9,10 +9,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ReadXML {
+    String file = "data.xml";
 
     public void readXML() throws JAXBException, FileNotFoundException {
         JAXBContext context2 = JAXBContext.newInstance(Operation.class);
-        Operation unmarshal = (Operation) context2.createUnmarshaller().unmarshal(new FileReader("data.xml"));
+        Operation unmarshal = (Operation) context2.createUnmarshaller().unmarshal(new FileReader(file));
         System.out.println(unmarshal);
     }
 }
--- src/main/java/com/calculator/storage/WriteXML.java
+++ src/main/java/com/calculator/storage/WriteXML.java
@@ -6,13 +6,17 @@ import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
 
 import java.io.File;
+import java.nio.file.Path;
 
 public class WriteXML {
+
+    String file = "data.xml";
+
 
     public void writeXML(Operation operation) throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(Operation.class);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        marshaller.marshal(operation, new File("data.xml"));
+        marshaller.marshal(operation, new File(file));
     }
 }
