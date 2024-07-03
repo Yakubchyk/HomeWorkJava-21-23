565faaa403df397ea4e6861c8b00c62f9b32dd84
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-07-03 16:46:33 +0200

    YES!

--- src/main/java/com/calculator/App.java
+++ src/main/java/com/calculator/App.java
@@ -15,35 +15,5 @@ public class App {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
-
-
-
-
-//        ConsoleApplication consoleApplication = new ConsoleApplication();
-//        consoleApplication.run();
-//
-//        CalculatorServer server = new CalculatorServer();
-//        try {
-//            server.startServer(consoleApplication.getOperation());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-
-
-//        Operation operation = new Operation();
-//        operation.setNum1(2);
-//        operation.setNum2(2);
-//        operation.setResult(4);
-//        operation.setOperator("sum");
-//
-//        JAXBContext context = JAXBContext.newInstance(Operation.class);
-//        Marshaller marshaller = context.createMarshaller();
-//        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-//        marshaller.marshal(operation, new File("data.xml"));
-
-//        JAXBContext context2 = JAXBContext.newInstance(Operation.class);
-//        Operation unmarshal = (Operation) context2.createUnmarshaller().unmarshal(new FileReader("data.xml"));
-//        System.out.println(unmarshal);
--- src/main/java/com/calculator/service/CalcHttpHandler.java
+++ src/main/java/com/calculator/service/CalcHttpHandler.java
@@ -1,11 +1,16 @@
 package com.calculator.service;
 
 import com.calculator.model.Operation;
+import com.calculator.storage.XMLFileOperationStorage;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import jakarta.xml.bind.JAXBException;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class CalcHttpHandler implements HttpHandler {
     private final Operation operation;
@@ -14,11 +19,36 @@ public class CalcHttpHandler implements HttpHandler {
         this.operation = operation;
     }
 
+    XMLFileOperationStorage xmlFileOperationStorage = new XMLFileOperationStorage();
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        exchange.getResponseHeaders().add("Content-Type", "text/html");
-        exchange.sendResponseHeaders(200, 0);
-        OutputStream os = exchange.getResponseBody();
+
+        String query = exchange.getRequestURI().getQuery();
+        Map<String, String> parameters = parseQuery(query);
+        OperationService operationService = new OperationService();
+
+        Operation operation = new Operation();
+        operation.setNum1(Double.parseDouble(parameters.get("num1")));
+        operation.setNum2(Double.parseDouble(parameters.get("num2")));
+        operation.setOperator(parameters.get("type"));
+
+
+        Operation result = operationService.getResult(operation);
+
+        try {
+            xmlFileOperationStorage.save(operation);
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            xmlFileOperationStorage.findAll();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
 
         String response = "<html><head><title>Calculator Result</title></head>" +
                 "<body>" +
@@ -28,8 +58,22 @@ public class CalcHttpHandler implements HttpHandler {
                 "<p>Operator : " + operation.getOperator() + "</p>" +
                 "<p>Result : " + operation.getResult() + "</p>" +
                 "</body></html>";
+        exchange.getResponseHeaders().add("Content-Type", "text/html");
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream os = exchange.getResponseBody();
 
         os.write(response.getBytes());
         os.close();
+    }
+
+    private Map<String, String> parseQuery(String query) {
+        Map<String, String> result = new HashMap<>();
+        String[] pairs = query.split("&");
+        for (String pair : pairs) {
+            String[] split = pair.split("=");
+            result.put(split[0], split[1]);
+        }
+        return result;
+
     }
 }
